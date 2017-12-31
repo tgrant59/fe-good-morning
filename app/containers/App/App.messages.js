4e45964c330fad56c85fb29669d48727ddd54c57
Tom Grant <t.grant@outlook.com>
2017-12-31 15:56:44 -0500

    Lint fixxxxx

--- app/containers/App/App.messages.js
+++ app/containers/App/App.messages.js
@@ -1,6 +1,6 @@
 export default {
     helmet: {
-        titleTemplate: '%s | Good Morning, Y\'all',
+        titleTemplate: "%s | Good Morning, Y'all",
         defaultTitle: 'Hungry?',
         description: 'Good morning Top Hatters, ya hungry?',
     },
