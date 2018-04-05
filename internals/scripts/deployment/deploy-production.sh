ad31b3c74b6213bbaaadd2c6a424b736b496374f
Tom Grant <tom.grant@tophatmonocle.com>
2018-04-05 14:26:44 -0400

    Fix Typo

--- internals/scripts/deployment/deploy-production.sh
+++ internals/scripts/deployment/deploy-production.sh
@@ -5,5 +5,5 @@ source "$DIR/../helpers/shell-helpers.sh"
 
 run "aws configure set preview.cloudfront true"
 
-run "aws s3 sync artifacts/build/ s3://good-morning.sunbusrt.io/"
-run "aws s3 cp artifacts/build/index.html s3://good-morning.sunbusrt.io/index.html --cache-control max-age=300"
+run "aws s3 sync artifacts/build/ s3://good-morning.sunburst.io/"
+run "aws s3 cp artifacts/build/index.html s3://good-morning.sunburst.io/index.html --cache-control max-age=300"
