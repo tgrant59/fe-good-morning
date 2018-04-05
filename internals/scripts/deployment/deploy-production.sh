9c69be2469596da8ed4bcc5a1cf2d50438682928
Tom Grant <tom.grant@tophatmonocle.com>
2018-04-05 14:05:27 -0400

    fix

--- internals/scripts/deployment/deploy-production.sh
+++ internals/scripts/deployment/deploy-production.sh
@@ -5,5 +5,5 @@ source "$DIR/../helpers/shell-helpers.sh"
 
 run "aws configure set preview.cloudfront true"
 
-run "aws s3 sync artifacts/build/ s3://fe-good-morning-demo/"
-run "aws s3 cp artifacts/build/index.html s3://fe-good-morning-demo/index.html --cache-control max-age=300"
+run "aws s3 sync artifacts/build/ s3://fe-good-morning-demo-open-house/"
+run "aws s3 cp artifacts/build/index.html s3://fe-good-morning-demo-open-house/index.html --cache-control max-age=300"
